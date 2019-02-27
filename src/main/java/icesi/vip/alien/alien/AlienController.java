3fbd8db54209c6870140dfd4e6576e5b8c6e1603
juan-carvajal <juan030698@hotmail.com>
2019-02-27 11:56:04 -0500

    Added changes to arquitecture

--- src/main/java/icesi/vip/alien/alien/AlienController.java
+++ src/main/java/icesi/vip/alien/alien/AlienController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import model.Constraint;
+import model.Model;
+import model.Solution;
+import model.Variable;
+import solver.interior_point.BarrierMethod;
+
 @RestController
 public class AlienController {
 
@@ -17,6 +23,21 @@ public class AlienController {
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@CrossOrigin
+	@RequestMapping("/test")
+	public Solution.ExportSolution test() throws Exception{
+		  Model m=new Model(Model.MAXIMIZE);
+	        m.addVariable("X1", Variable.CONTINUOUS, 2);
+	        m.addVariable("X2", Variable.CONTINUOUS, -1);
+	        m.addVariable("X3", Variable.CONTINUOUS, 2);
+	        m.addConstraint(new double[]{2,1,0}, Constraint.LESS_OR_EQUAL, 10, "");
+	        m.addConstraint(new double[]{1,2,-2}, Constraint.LESS_OR_EQUAL, 20, "");
+	        m.addConstraint(new double[]{0,1,2}, Constraint.LESS_OR_EQUAL, 5, "");
+	        BarrierMethod bm=new BarrierMethod();
+	        return bm.solve(m).exportFormat();
+		
 	}
 
 }
