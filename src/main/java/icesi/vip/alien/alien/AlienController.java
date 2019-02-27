296951bbde166f2669eae01cbb63abba22930242
juan-carvajal <juan030698@hotmail.com>
2019-02-27 13:12:55 -0500

    Added test parameters

--- src/main/java/icesi/vip/alien/alien/AlienController.java
+++ src/main/java/icesi/vip/alien/alien/AlienController.java
@@ -27,14 +27,31 @@ public class AlienController {
 	
 	@CrossOrigin
 	@RequestMapping("/test")
-	public Solution.ExportSolution test() throws Exception{
-		  Model m=new Model(Model.MAXIMIZE);
-	        m.addVariable("X1", Variable.CONTINUOUS, 2);
-	        m.addVariable("X2", Variable.CONTINUOUS, -1);
-	        m.addVariable("X3", Variable.CONTINUOUS, 2);
-	        m.addConstraint(new double[]{2,1,0}, Constraint.LESS_OR_EQUAL, 10, "");
-	        m.addConstraint(new double[]{1,2,-2}, Constraint.LESS_OR_EQUAL, 20, "");
-	        m.addConstraint(new double[]{0,1,2}, Constraint.LESS_OR_EQUAL, 5, "");
+	public Solution.ExportSolution test(@RequestParam(value="type",required=true)String type,@RequestParam(value="vars",required=true) String vars, @RequestParam(value="objectiveFunction",required=true)String objectiveFunction,@RequestParam(value="constraints",defaultValue="")String constraints) throws Exception{
+		  
+		
+		Model m=new Model(type);
+		String[]varsS=vars.split(",");
+		String[]coef=objectiveFunction.split(",");
+		String[]cons=constraints.split(";");
+		for (int i = 0; i < varsS.length; i++) {
+			m.addVariable(varsS[i].split(":")[0], varsS[i].split(":")[1], Double.parseDouble(coef[i]));
+		}
+		int varcount=varsS.length;
+		for (int i = 0; i < cons.length; i++) {
+			double[]c=new double[varcount];
+			String[]cons2=cons[i].split(",");
+			for (int j = 0; j < c.length; j++) {
+				c[j]=Double.parseDouble(cons[j]);
+			}
+			m.addConstraint(c, cons2[cons2.length-2], Double.parseDouble(cons2[cons2.length-1]), "C"+i);
+		}
+//	        m.addVariable("X1", Variable.CONTINUOUS, 2);
+//	        m.addVariable("X2", Variable.CONTINUOUS, -1);
+//	        m.addVariable("X3", Variable.CONTINUOUS, 2);
+//	        m.addConstraint(new double[]{2,1,0}, Constraint.LESS_OR_EQUAL, 10, "");
+//	        m.addConstraint(new double[]{1,2,-2}, Constraint.LESS_OR_EQUAL, 20, "");
+//	        m.addConstraint(new double[]{0,1,2}, Constraint.LESS_OR_EQUAL, 5, "");
 	        BarrierMethod bm=new BarrierMethod();
 	        return bm.solve(m).exportFormat();
 		
