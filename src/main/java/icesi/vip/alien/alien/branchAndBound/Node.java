a4e447584f7c33d32e5abafb07ea173d6b46d29f
juan-carvajal <39658930+juan-carvajal@users.noreply.github.com>
2019-04-19 18:11:07 -0500

    Update B&B

--- src/main/java/icesi/vip/alien/alien/branchAndBound/Node.java
+++ src/main/java/icesi/vip/alien/alien/branchAndBound/Node.java
@@ -7,82 +7,53 @@ import model.Solution;
 import solver.integer.branch_and_bound.BranchAndBound.ModelNode;
 
 public class Node {
-	
-	
+
 	public ArrayList<Node> children;
 	public NodeText text;
-	
-	
-	
-	
 
-
-	
-	
-	
 	public ArrayList<Node> getChildren() {
 		return children;
 	}
-
-
-
-
-
-
-
-
 
 	public NodeText getText() {
 		return text;
 	}
 
-
-
-
-
-
-
-
-
 	public Node(ModelNode copy) {
 		try {
-			
-			children=new ArrayList<Node>();
-			
-			text=new NodeText("Solution at Level "+copy.level, copy.model.toString(),copy.solution.toString());
 
+			if (copy != null) {
+				children = new ArrayList<Node>();
 
-			
-			if(copy.left!=null) {
-				children.add(new Node(copy.left));
-				
+				text = new NodeText("Solution at Level " + copy.level, copy.model.toString(), copy.solution.toString());
+
+				if (copy.left != null) {
+					children.add(new Node(copy.left));
+
+				}
+
+				if (copy.right != null) {
+					children.add(new Node(copy.right));
+				}
+			} else {
+				children = new ArrayList<Node>();
+
+				text = new NodeText("UNFEASIBLE BRANCH","", "");
+
+				if (copy.left != null) {
+					children.add(new Node(copy.left));
+
+				}
+
+				if (copy.right != null) {
+					children.add(new Node(copy.right));
+				}
 			}
-			
-			if(copy.right!=null) {
-				children.add(new Node(copy.right));
-			}
-			
 
-			
-			
-			
-			
-			
-			
-		
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
-
-
-
-
-
-	
-	
-	
 
 }
